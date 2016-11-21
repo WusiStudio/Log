18145195f832d0f78b66c98b15251c66e8a7ea9b
wusidn <wusidn@163.com>
2016-11-21 15:19:43 +0800

    初始化

---
+++ src/Log.cpp
@@ -0,0 +1,296 @@
+#include "Log.h"
+#include <random>
+
+namespace WsTools
+{
+    string Log::_levelName[]  = { "DEBUG", "INFO", "NOTICE", "WARN", "ERR", "CRIT", "ALERT", "EMERG" };
+
+    map<string, string> Log::escapes;
+
+    mutex Log::mtx;
+
+    Log::Log(void)
+    {
+        _filter = level::DEBUG;
+    }
+
+    void Log::setFilterLevel(const level _level)
+    {
+        _filter = _level;
+    }
+
+    void Log::printLog(const string & level, const string & log) const
+    {
+        cout << level << log << endl;
+    }
+
+    void Log::_log(const level _level, const string & str) const
+    {
+        if(_level < _filter) return;
+        //输出打印信息
+        _printLog(_level, str);
+    }
+
+    bool Log::_format_D(stringstream & strs, const string & format, const string & source) const
+    {
+        static regex format_D("^[dD](\\d*)$");
+        auto matchBegin = sregex_iterator(format.begin(), format.end(), format_D);
+        auto matchEnd = sregex_iterator();
+
+        if(matchBegin == matchEnd){ return false; }
+        
+        int minCount = atoi(matchBegin->str(1).c_str());
+
+        static regex checkNumber("^[+-]?(\\d+(\\.\\d+)?)(e\\+\\d+)?$|^[+-]?\\d{1,3}(,\\d{3})*(\\.(\\d{3},)*\\d{1,3})?(e\\+\\d+)?$");
+        matchBegin = sregex_iterator(source.begin(), source.end(), checkNumber);
+
+        if(matchBegin == matchEnd)
+        {
+            error("格式化数据不匹配");
+            strs << format;
+            return true;
+        }
+        
+        string sourceData = matchBegin->str();
+        int convertData;
+        
+        for(auto index = sourceData.find(','); index != string::npos; index = sourceData.find(','))
+        {
+            sourceData.replace(sourceData.begin() + index, sourceData.begin() + index + 1, "");
+        }
+
+        if(sourceData.find('.') != string::npos)
+        {
+            convertData = (int)roundf(atof(sourceData.c_str()));
+        }else
+        {
+            convertData = atoi(sourceData.c_str());
+        }
+
+        stringstream tempSStr;
+        tempSStr << convertData;
+
+        for(int i = tempSStr.str().length(); i < minCount; ++i){ strs << "0"; }
+        strs << convertData;
+
+        return true;
+    }
+
+    bool Log::_format_C(stringstream & strs, const string & format, const string & source) const
+    {
+        static regex format_D("^[cC](\\d*)$");
+        auto matchBegin = sregex_iterator(format.begin(), format.end(), format_D);
+        auto matchEnd = sregex_iterator();
+        if(matchBegin == matchEnd){ return false; }
+
+        int decimalDigits = 2;
+        if(matchBegin->str(1).length() > 0){ decimalDigits = atoi(matchBegin->str(1).c_str()); }
+
+        static regex checkNumber("^([^-+]?)([+-]?\\d+(\\.\\d+)?(e\\+\\d+)?)$|^([^-+]?)([+-]?\\d{1,3}(,\\d{3})*(\\.(\\d{3},)*\\d{1,3})?(e\\+\\d+)?)$");
+        matchBegin = sregex_iterator(source.begin(), source.end(), checkNumber);
+
+        if(matchBegin == matchEnd)
+        {
+            error("尝试格式化失败");
+            strs << format;
+            return true;
+        }
+
+        string unit = matchBegin->str(1).length() > 0 ? matchBegin->str(1) : "￥";
+
+        string sourceData = matchBegin->str(2);
+
+        for(auto index = sourceData.find(','); index != string::npos; index = sourceData.find(','))
+        {
+            sourceData.replace(sourceData.begin() + index, sourceData.begin() + index + 1, "");
+        }
+
+        float convertData = roundf(atof(sourceData.c_str()) * pow(10, decimalDigits)) / pow(10, decimalDigits);
+
+        stringstream tempSStr;
+        tempSStr << convertData;
+
+        //科学计数法转换
+        string tempStr = scientificToDefault(tempSStr.str());
+        strs << unit << tempStr;
+
+        auto dotIndex = tempStr.find('.');
+        if(dotIndex == string::npos)
+        {
+            strs << ".";
+            dotIndex = tempStr.length() - 1;
+        }
+
+        for(int i = tempStr.length() - dotIndex - 1; i < decimalDigits; ++i){ strs << "0"; }
+                    
+        return true;
+    }
+
+    bool Log::_format_F(stringstream & strs, const string & format, const string & source) const
+    {
+        static regex format_D("^[fF](\\d*)$");
+        auto matchBegin = sregex_iterator(format.begin(), format.end(), format_D);
+        auto matchEnd = sregex_iterator();
+
+        if(matchBegin == matchEnd){ return false; }
+
+        int decimalDigits = 2;
+        if(matchBegin->str(1).length() > 0)
+        {
+            decimalDigits = atoi(matchBegin->str(1).c_str());
+        }
+
+        static regex checkNumber("^([+-]?\\d+(\\.\\d+)?)(e[\\+-]\\d+)?$|^[+-]?\\d{1,3}(,\\d{3})*(\\.(\\d{3},)*\\d{1,3})?(e[\\+-]\\d+)?$");
+        matchBegin = sregex_iterator(source.begin(), source.end(), checkNumber);
+        if(matchBegin == matchEnd)
+        {
+            error("尝试格式化失败");
+            strs << format;
+            return true;
+        }
+
+        string sourceData = matchBegin->str();
+
+        for(auto index = sourceData.find(','); index != string::npos; index = sourceData.find(','))
+        {
+            sourceData.replace(sourceData.begin() + index, sourceData.begin() + index + 1, "");
+        }
+
+        double convertData = round(atof(sourceData.c_str()) * pow(10, decimalDigits)) / pow(10, decimalDigits);
+
+        stringstream tempSStr;
+        tempSStr << convertData;
+
+        //科学计数法转换
+        string tempStr = scientificToDefault(tempSStr.str());
+        strs << tempStr;
+
+        auto dotIndex = tempStr.find('.');
+        if(dotIndex == string::npos)
+        {
+            strs << ".";
+            dotIndex = tempStr.length() - 1;
+        }
+
+        for(int i = tempStr.length() - dotIndex - 1; i < decimalDigits; ++i){ strs << "0"; }
+        return true;
+    }
+
+    bool Log::_format_aline(stringstream & strs, const string & format, const string & source) const
+    {
+        static regex format_D("^([-]?)(\\d*)$");
+        auto matchBegin = sregex_iterator(format.begin(), format.end(), format_D);
+        auto matchEnd = sregex_iterator();
+        if(matchBegin == matchEnd){ return false; }
+
+        int minCount = atoi(matchBegin->str(2).c_str());
+
+        if(matchBegin->str(1).c_str() == string("-"))
+        {
+            for(int i = getStringLength(source); i < minCount; ++i)
+            {
+                strs << " ";
+            }
+        }
+
+        strs << source;
+
+        if(matchBegin->str(1).c_str() != string("-"))
+        {
+            for(int i = getStringLength(source); i < minCount; ++i)
+            {
+                strs << " ";
+            }
+        }
+
+        return true;
+    }
+
+
+    void Log::_printLog(const level _level, const string & log) const
+    {
+        stringstream sstr;
+        sstr << "[" << _levelName[_level] << "] ";
+        //加线程锁锁
+        unique_lock<mutex> lck(mtx, defer_lock);
+        lck.lock();
+            printLog(sstr.str(), log);
+        lck.unlock();
+    }
+
+    string Log::scientificToDefault(const string & source) const
+    {
+        string result = source;
+        //处理科学计数法
+        auto scientificIndex = result.find("e+");
+        if(scientificIndex != string::npos)
+        {
+            string baseNumber = result.substr(0, scientificIndex);
+            int exponential = atoi(result.substr(scientificIndex + 2).c_str());
+            auto dotIndex = baseNumber.find('.');
+            baseNumber.replace(dotIndex, dotIndex + 1, "");
+            while(baseNumber.length() < exponential + dotIndex)
+            {
+                baseNumber += "0";
+            }
+            result = baseNumber;
+        }
+
+        scientificIndex = result.find("e-");
+        if(scientificIndex != string::npos)
+        {
+            string baseNumber = result.substr(0, scientificIndex);
+            int exponential = atoi(result.substr(scientificIndex + 2).c_str());
+            auto dotIndex = baseNumber.find('.');
+            baseNumber.replace(dotIndex, dotIndex + 1, "");
+
+            string tempHead = "";
+            while((int)tempHead.length() + 1 < exponential)
+            {
+                tempHead += "0";
+            }
+            result = "0." + tempHead + baseNumber;
+        }
+        return result;
+    }
+
+    string Log::createUuid(void) const
+    {
+        char temp[33] = {0};
+        for(size_t i = 0; i < (sizeof(temp) - 1) / 2; ++i)
+        {
+            random_device rd;
+            uniform_int_distribution<int> dis(0x00, 0xFF);
+            sprintf(temp + 2 * i, "%02X", dis(rd));
+        }
+        return temp;
+    }
+
+    //返回字符串实际长度（单位是一个英文字符）
+    const unsigned Log::getStringLength(const string & str, const string & coding) const
+    {
+        unsigned result = 0;
+        if(coding == "utf-8")
+        {
+            //根据utf-8编码规则判断一个字符占几个字节
+            for(size_t i = 0; i < str.length();)
+            {
+                int length = 0;
+                unsigned short temp = str.at(i) & 0x00FF;
+                while((temp << length & 0x00FF) > 1 << 7)
+                {
+                    length ++;
+                }
+                //大于０x80的字符是中文并且占两个英文字符的宽度
+                result += temp < 0x80 ? 1 : 2;
+                length = length < 1 ? 1 : length;
+                i += length;
+            }
+        }else
+        {
+            //其他编码类型还没有做
+            result = str.length();
+        }
+        return result;
+    }
+}
---
+++ test/LogTest.cpp
@@ -0,0 +1,63 @@
+#include <Log.h>
+
+using namespace WsTools;
+
+int main(int argc, char ** argv)
+{
+    Log ws_log;
+
+    //只打印字符串
+    ws_log.info("只打印字符串");
+
+    //占位符
+    ws_log.info("占位符: {0}", "字符串");
+
+    //占位符复用
+    ws_log.info("占位符复用: {0}, {0}", "字符串");
+
+    //字符串长度
+    ws_log.info("字符串长度: {0, 10}{0}", "字符串");
+    ws_log.info("字符串长度: {0, -10}", "字符串");
+
+    //数字
+    ws_log.info("数字: {0}", 12);
+
+    //数字格式化
+    ws_log.info("数字格式化: {0, D}", 12);
+    ws_log.info("数字格式化: {0, D3}", 12);
+    ws_log.info("数字格式化: {0, D3}", 12.56);
+
+    //小数
+    ws_log.info("小数: {0, F}", "12.345");
+    ws_log.info("小数: {0, F}", 12.345);
+    ws_log.info("小数: {0, F1}", 12.345);
+
+    //金钱
+    ws_log.info("t金钱est: {0, C4}", "$12.34567");
+
+    //设置日志级别
+    
+    ws_log.info("设置日志级别：DEBUG");
+    ws_log.setFilterLevel(Log::level::DEBUG);
+    ws_log.debug(".");
+    ws_log.info(".");
+    ws_log.notice(".");
+    ws_log.warning(".");
+    ws_log.error(".");
+    ws_log.critical(".");
+    ws_log.alert(".");
+    ws_log.emerg(".");
+
+    ws_log.info("设置日志级别：ERR");
+    ws_log.setFilterLevel(Log::level::ERR);
+    ws_log.debug(".");
+    ws_log.info(".");
+    ws_log.notice(".");
+    ws_log.warning(".");
+    ws_log.error(".");
+    ws_log.critical(".");
+    ws_log.alert(".");
+    ws_log.emerg(".");
+    
+    return 0;
+}
